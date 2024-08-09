4899c3fe1eb268d7aacb820dcfb6fa82e46feaf9
Hepatica <danil.tkachenko@getmore.cz>
2024-08-09 19:08:09 +0200

    Add project files.

---
+++ SpCloudMain/SpCloudMain.cpp
@@ -0,0 +1,15 @@
+﻿// SpCloudMain.cpp : Defines the entry point for the application.
+//
+
+#include "SpCloudMain.h"
+
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		cout << "Hello CMake." << endl;
+	}
+	return 0;
+}
